5ec1f6c846fbb3d90635ba1c75f8671dc22702c6
Guiii-lz <Guima.luiz@hotmail.com>
2021-08-11 23:46:53 -0300

    Add project files.

---
+++ Composition-Enum-Course/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition_Enum_Course.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
